3f82ab50b05ee9b7af9761ce08e62b8eb7502ac8
7mud2007a <bznsman77@gmail.com>
2026-09-11 19:13:16 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,40 @@
-import React from 'react';
-import Navbar from './components/Navbar';
-import Hero from './components/Hero';
-import WhyUs from './components/WhyUs';
-import Services from './components/Services';
-import Occasions from './components/Occasions';
-import Gallery from './components/Gallery';
-import Contact from './components/Contact';
-import Location from './components/Location';
-import CTA from './components/CTA';
-import Footer from './components/Footer';
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import WhyUs from "./components/WhyUs";
+import Services from "./components/Services";
+import Occasions from "./components/Occasions";
+import Gallery from "./components/Gallery";
+import Contact from "./components/Contact";
+import Location from "./components/Location";
+import CTA from "./components/CTA";
+import Footer from "./components/Footer";
 
-export default function App() {
+function App() {
   return (
-    <div className="min-h-screen bg-[#1A0F0B] text-[#FAF6EE] font-arabic selection:bg-[#D4AF37] selection:text-[#1A0F0B]">
+    <div className="min-h-screen overflow-x-hidden bg-[#241006] text-[#FFF7E8]">
       <Navbar />
+
       <main>
         <Hero />
+
         <WhyUs />
+
         <Services />
+
         <Occasions />
+
         <Gallery />
+
         <Contact />
+
         <Location />
+
         <CTA />
       </main>
+
       <Footer />
     </div>
   );
 }
+
+export default App;
